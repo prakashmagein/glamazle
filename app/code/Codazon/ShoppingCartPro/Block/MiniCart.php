7a7c7cc3dddc6acc49c2e43f96d50a97534639cf
Prakash Kumar <prakash.magein@gmail.com>
2025-10-23 14:34:05 +0530

    Add caching metadata for cart and shipping blocks

--- app/code/Codazon/ShoppingCartPro/Block/MiniCart.php
+++ app/code/Codazon/ShoppingCartPro/Block/MiniCart.php
@@ -5,23 +5,127 @@
  */
 namespace Codazon\ShoppingCartPro\Block;
 
-class MiniCart extends \Magento\Framework\View\Element\Template
+use Codazon\ShoppingCartPro\Helper\Data as ShoppingCartHelper;
+use Magento\Checkout\Model\Session as CheckoutSession;
+use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Framework\DataObject\IdentityInterface;
+use Magento\Framework\Serialize\Serializer\Json;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Quote\Model\Quote as QuoteEntity;
+
+class MiniCart extends Template implements IdentityInterface
 {
-    
-    /* @var \Codazon\ShoppingCartPro\Helper\Data */
+    private const CACHE_LIFETIME = 600;
+
+    /**
+     * @var ShoppingCartHelper
+     */
     protected $helper;
-    
+
+    /**
+     * @var CheckoutSession
+     */
+    private $checkoutSession;
+
+    /**
+     * @var CustomerSession
+     */
+    private $customerSession;
+
+    /**
+     * @var Json
+     */
+    private $serializer;
+
     public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context,
-        \Codazon\ShoppingCartPro\Helper\Data $helper,
+        Context $context,
+        ShoppingCartHelper $helper,
+        CheckoutSession $checkoutSession,
+        CustomerSession $customerSession,
+        Json $serializer,
         array $data = []
-    ){
+    ) {
         $this->helper = $helper;
+        $this->checkoutSession = $checkoutSession;
+        $this->customerSession = $customerSession;
+        $this->serializer = $serializer;
         parent::__construct($context, $data);
     }
-        
-    public function getHelper()
+
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->setCacheLifetime(self::CACHE_LIFETIME);
+    }
+
+    public function getHelper(): ShoppingCartHelper
     {
         return $this->helper;
+    }
+
+    public function getIdentities(): array
+    {
+        $quoteId = $this->getQuoteId();
+        $identities = [QuoteEntity::CACHE_TAG];
+
+        if ($quoteId) {
+            $identities[] = QuoteEntity::CACHE_TAG . '_' . $quoteId;
+        }
+
+        return $identities;
+    }
+
+    public function getCacheKeyInfo(): array
+    {
+        $quote = $this->getQuote();
+        $quoteId = $quote ? (int) $quote->getId() : 0;
+        $itemsHash = $quote ? md5($this->serializer->serialize($this->buildItemsSignature($quote))) : '';
+        $totalsHash = $quote
+            ? md5($this->serializer->serialize([
+                'subtotal' => (float) $quote->getSubtotal(),
+                'grand_total' => (float) $quote->getGrandTotal(),
+                'items_qty' => (float) $quote->getItemsQty(),
+            ]))
+            : '';
+
+        return [
+            'CODAZON_MINICARTPRO',
+            (int) $this->_storeManager->getStore()->getId(),
+            $this->_storeManager->getStore()->getCurrentCurrencyCode(),
+            $this->customerSession->getCustomerGroupId(),
+            $quoteId,
+            $itemsHash,
+            $totalsHash,
+        ];
+    }
+
+    private function getQuoteId(): ?int
+    {
+        $quote = $this->getQuote();
+        return $quote ? (int) $quote->getId() : null;
+    }
+
+    private function getQuote(): ?QuoteEntity
+    {
+        $quote = $this->checkoutSession->getQuote();
+        return $quote instanceof QuoteEntity ? $quote : null;
+    }
+
+    private function buildItemsSignature(QuoteEntity $quote): array
+    {
+        $items = [];
+
+        foreach ($quote->getAllVisibleItems() as $item) {
+            $items[] = [
+                'id' => (int) $item->getId(),
+                'sku' => (string) $item->getSku(),
+                'qty' => (float) $item->getQty(),
+                'row_total' => (float) $item->getRowTotal(),
+                'updated_at' => (string) $item->getUpdatedAt(),
+            ];
+        }
+
+        return $items;
     }
 }
